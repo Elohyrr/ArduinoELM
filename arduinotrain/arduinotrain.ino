f0d02ed86da7714c9788350bd0eade2aa9e4b894
Elohyrr <leohery888@hotmail.fr>
2023-11-27 10:36:27 +0100

    test

--- arduinotrain/arduinotrain.ino
+++ arduinotrain/arduinotrain.ino
@@ -1,9 +1,29 @@
+#include "rgb_lcd.h"
+#include "Wire.h"
+#define Led 2
+#define button 6
+
+void clignote();
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(button, INPUT);
+  pinMode(Led, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int valeur_button = digitalRead(button);
+  if(valeur_button == 0){
+      clignote();
+  }else{
+    digitalWrite(Led,0);
+  }
+}
 
+void clignote(){
+  delay(2000);
+  digitalWrite(Led, 1);
+  delay(2000);
+  digitalWrite(Led, 0);
 }
